17b3f3d2572911b2db409a5f67cb06037906c703
Moritz Hein <moritz.hein@live.de>
2022-09-05 14:57:06 +0200

    Try fixing node specific imports by exluding in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		include: ['highlight.js', 'highlight.js/lib/core']
+		include: ['highlight.js', 'highlight.js/lib/core'],
+		exclude: ['node:fs/promises', 'node:path', 'node:url']
 	}
 };
 
