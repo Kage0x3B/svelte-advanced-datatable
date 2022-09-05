1d4595703dd253e27e95a58c4432587edfa91793
Moritz Hein <moritz.hein@live.de>
2022-09-05 14:42:04 +0200

    Don't fail ts build if api reference meta doesn't exist

--- src/routes/(docs)/api-reference/util/apiReferenceMeta.ts
+++ src/routes/(docs)/api-reference/util/apiReferenceMeta.ts
@@ -1,3 +1,5 @@
+// @ts-ignore - ignore if the file doesn't exist
 import apiReferenceJson from '../files/api-reference-meta.json';
 
+// @ts-ignore
 export const apiReferenceCategories = apiReferenceJson.children.map((c) => c.name);
