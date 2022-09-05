5d6b8a2399ce4982c8b844896ef6e2471a5a6716
Moritz Hein <moritz.hein@live.de>
2022-09-05 16:31:41 +0200

    Try fixing node specific imports by also excluding external deps from rollup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['node:fs', 'node:fs/promises', 'node:path', 'node:url', 'fs', 'fs/promises', 'path', 'url']
+		}
+	},
 	optimizeDeps: {
 		include: ['highlight.js', 'highlight.js/lib/core'],
 		exclude: ['node:fs/promises', 'node:path', 'node:url', 'fs', 'fs/promises', 'path', 'url'],
