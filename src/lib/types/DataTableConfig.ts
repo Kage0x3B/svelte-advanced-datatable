af48d67721279990c16e244fc309844f71dabfd8
Moritz Hein <moritz.hein@live.de>
2022-09-07 18:15:54 +0200

    Write tsdoc for the DataTableConfig interface

--- src/lib/types/DataTableConfig.ts
+++ src/lib/types/DataTableConfig.ts
@@ -30,32 +30,31 @@ export type MessageConfig<T extends DataRecord = DataRecord> = Record<keyof T, C
 };
 
 export interface DataTableConfig<T extends DataRecord = DataRecord> {
+	/**
+	 * A unique identifier/name for this datatable. Should not contain whitespaces and non-ascii characters
+	 */
 	type: string;
 
+	/**
+	 * An object with one key for each key in the data, containing configuration options for each table column
+	 */
 	columnProperties: TableColumnConfig<T>;
 
-	modalComponent?: SvelteComponentTyped;
-
-	onItemClick?: (item: T) => void;
-
+	/**
+	 * The data source where the datatable requests the table data from
+	 */
 	dataSource: IDataSource<T> | Readable<IDataSource<T>>;
 
-	forcedSearchQuery?: ForcedSearchQuery<T>;
-
+	/**
+	 * The key of your items unique identifier.
+	 *
+	 * For example a user id or a counter value which increases by one for each item, as long as it's unique for each item
+	 */
 	dataUniquePropertyKey: keyof T & string;
 
-	highlightedItemId?: string | Readable<string | undefined>;
-
-	defaultSort?: { columnKey?: keyof T & string; direction?: SortDirection };
-
-	showPagination?: boolean;
-
-	showTopPagination?: boolean;
-
-	showBottomPagination?: boolean;
-
-	itemsPerPage?: number;
-
+	/**
+	 * Whether to use the messageConfig or the svelte-i18n library to provide all strings used by the datatable
+	 */
 	messageFormatterType?: 'config' | 'svelte-i18n';
 
 	/**
@@ -63,12 +62,76 @@ export interface DataTableConfig<T extends DataRecord = DataRecord> {
 	 */
 	messageFormatterPrefix?: string;
 
+	/**
+	 * An object containing all strings used by the datatable, such as table headers, titles of buttons and more.
+	 *
+	 * Ignored if svelte-i18n is enabled by the `messageFormatterType` option
+	 */
 	messageConfig?: MessageConfig<T>;
 
+	/**
+	 * A custom message formatter which can return a replacement or undefined to default to the message provided by the internal formatter/svelte-i18n
+	 */
 	additionalMessageFormatter?: MessageFormatter;
 
+	/**
+	 * A svelte component shown when a user clicks on a row to expand it
+	 */
+	modalComponent?: SvelteComponentTyped;
+
+	/**
+	 * An onClick handler for a table row. Gets passed the data item which the clicked row displays
+	 * @param item
+	 */
+	onItemClick?: (item: T) => void;
+
+	/**
+	 * A search query which overwrites any values by the users current search. Can be used to apply a forced filter to the whole datatable
+	 */
+	forcedSearchQuery?: ForcedSearchQuery<T>;
+
+	/**
+	 * The identifier of any item which then gets assigned the `highlighted` class
+	 */
+	highlightedItemId?: string | Readable<string | undefined>;
+
+	/**
+	 * Sort the table using the given key and direction by default
+	 */
+	defaultSort?: { columnKey?: keyof T & string; direction?: SortDirection };
+
+	/**
+	 * Whether to enable or disable pagination entirely.
+	 *
+	 * Watch out that the server must send all table rows at once if this is disabled.
+	 */
+	enablePagination?: boolean;
+
+	/**
+	 * If the pagination component at the top of the datatable should be shown
+	 */
+	showTopPagination?: boolean;
+
+	/**
+	 * If the pagination component at the bottom of the datatable should be shown.
+	 *
+	 * Notice that the bottom pagination is always hidden when less than 10 rows are shown
+	 */
+	showBottomPagination?: boolean;
+
+	/**
+	 * Maximum amount of rows shown on one page
+	 */
+	itemsPerPage?: number;
+
+	/**
+	 * Whether to show the search textbox
+	 */
 	showSearch?: boolean;
 
+	/**
+	 * Which search parser to use to parse the users search text into search filters, categories and more
+	 */
 	searchParser?: ISearchParser;
 }
 
