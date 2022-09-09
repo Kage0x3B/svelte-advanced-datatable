931910e3054f2f818b75f4bdc741f7efe34322a8
Moritz Hein <moritz.hein@live.de>
2022-09-09 21:04:22 +0200

    Improve homepage

--- src/routes/(docs)/+page.svelte
+++ src/routes/(docs)/+page.svelte
@@ -7,7 +7,12 @@
 </svelte:head>
 
 <Container class='pt-6'>
-	<h1 class='mb-6'>Svelte Advanced Datatable</h1>
-	<Button href='/docs/getting-started' color='primary'>Explore docs</Button>
-	<Button href='/api-reference' color='primary' outline>API Reference</Button>
+	<div class='mb-6'>
+		<h1 class='display-1'>Svelte Advanced Datatable</h1>
+		<p class='lead'>
+			A highly configurable datatable for Svelte and Sveltestrap UI components (and more soon)
+		</p>
+	</div>
+	<Button href='/docs/getting-started' color='primary' size='lg'>Explore docs</Button>
+	<Button href='/api-reference' color='primary' outline size='lg'>API Reference</Button>
 </Container>
